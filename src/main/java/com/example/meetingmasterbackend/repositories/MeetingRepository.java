29d58e91900408eb31dd0320728c16d5c0bc537e
Diaeddin BOUIDAINE <diaeddin.bouidaine@zsoft-consulting.com>
2023-02-22 16:07:36 +0100

    build repositories

--- src/main/java/com/example/meetingmasterbackend/repositories/MeetingRepository.java
+++ src/main/java/com/example/meetingmasterbackend/repositories/MeetingRepository.java
@@ -3,5 +3,10 @@ package com.example.meetingmasterbackend.repositories;
 import com.example.meetingmasterbackend.models.Meeting;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MeetingRepository extends CrudRepository<Meeting,Long> {
+
+    List<Meeting> findAll();
+    Meeting findMeetingById(long id);
 }
--- src/main/java/com/example/meetingmasterbackend/repositories/ProfileRepository.java
+++ src/main/java/com/example/meetingmasterbackend/repositories/ProfileRepository.java
@@ -3,5 +3,11 @@ package com.example.meetingmasterbackend.repositories;
 import com.example.meetingmasterbackend.models.Profile;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProfileRepository extends CrudRepository<Profile,Long> {
+    List<Profile> findAll();
+    Profile findProfileById(long id);
+    Profile findProfileByEmail(String email);
+    List<Profile> findAllByNameContains(String name);
 }
